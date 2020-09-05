8ceeb920cb31e0a0672a575a305de2a61358a02d
unknown <jame_glove@yahoo.com>
2020-09-06 04:12:05 +0800

    apaan ni yak, coba enable dulu

--- inc/pages/Beatmaps.php
+++ inc/pages/Beatmaps.php
@@ -6,11 +6,11 @@ class Beatmaps {
 	const Title = 'Ripple - Carroponte';
 
 	public function P() {
-		echo('<iframe width="560" height="315" src="https://www.youtube.com/embed/G_QfYsmNIHQ?autoplay=1" frameborder="0" allowfullscreen></iframe><br>');
+		/*echo('<iframe width="560" height="315" src="https://www.youtube.com/embed/G_QfYsmNIHQ?autoplay=1" frameborder="0" allowfullscreen></iframe><br>');
 		for ($i=0; $i < 100; $i++) {
 			echo '<h3 class="carroponte" hidden>O-oooooooooo-AAAAE-A-A-I-A-U-JO-oooooooooooo-AAE-O-A-A-U-U-A-E-eee-ee-eee-AAAAE-A-E-I-E-A-JO-ooo-oo-oo-oo-EEEEO-A-AAA-AAAA</h3>';
-		}
-		/*
+		}*/
+		
 		$beatmaps = $GLOBALS["db"]->fetchAll("SELECT* FROM beatmaps WHERE ranked >= 2 GROUP BY beatmapset_id ORDER BY latest_update DESC LIMIT 51");
 		$c = 0;
 		P::GlobalAlert();
@@ -94,6 +94,6 @@ class Beatmaps {
 
 			echo '</div>
 			</div>
-		</div>';*/
+		</div>';
 	}
 }
