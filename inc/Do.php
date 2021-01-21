0a335067f44b4bef451b7ffd2479ea8db0cd1d9e
troke12 <jame_glove@yahoo.com>
2021-01-21 14:52:57 +0000

    besok lanjutin

--- inc/Do.php
+++ inc/Do.php
@@ -1046,8 +1046,8 @@ class D {
 				$bm = $GLOBALS["db"]->fetch("SELECT beatmapset_id, song_name FROM beatmaps WHERE beatmapset_id = ? LIMIT 1", [$bsid]);
 
 				$msg = "[https://osu.ppy.sh/s/" . $bsid . " " . $bm["song_name"] . "] is now ranked!";
-				$to = "%23ranked";
-				$requesturl = $URL["bancho"] . "/api/v1/fokabotMessage?k=" . urlencode($ScoresConfig["api_key"]) . "&to=" . urlencode($to) . "&msg=" . urlencode($msg);
+				$to = "%23ranked-now";
+				$requesturl = $URL["bancho"] . "/api/v1/fokabotMessage?k=" . $ScoresConfig["api_key"] . "&to=" . urlencode($to) . "&msg=" . urlencode($msg);
 				$resp = getJsonCurl($requesturl);
 	
 				if ($resp["message"] != "ok") {
@@ -1413,21 +1413,21 @@ class D {
 				$statuscrot = "Ranked";
 				$bpm = $bm["bpm"];
 				$namabm = $bm["song_name"];
-				$requestbro = $URL["bancho"] . "/api/v1/fokabotMessage?k=" . urlencode($ScoresConfig["api_key"]) . "&to=%23ranked-now&msg=" . $msgtoannounce . "";
+				$requestbro = $URL["bancho"] . "/api/v1/fokabotMessage?k=" . $ScoresConfig["api_key"] . "&to=%23ranked-now&msg=" . $msgtoannounce . "";
 			} else if ($status == "love") {
 				$bm = $GLOBALS["db"]->fetch("SELECT beatmapset_id, song_name, bpm FROM beatmaps WHERE beatmapset_id = ? LIMIT 1", [$bsid]);
 				$msgtoannounce = "[https://osu.ppy.sh/s/" . $bsid . " " . $bm["song_name"] . "] is now Loved!";
 				$statuscrot = "Loved";
 				$bpm = $bm["bpm"];
 				$namabm = $bm["song_name"];
-                $requestbro = $URL["bancho"] . "/api/v1/fokabotMessage?k=" . urlencode($ScoresConfig["api_key"]) . "&to=%23ranked-now&msg=" . $msgtoannounce . "";
+                		$requestbro = $URL["bancho"] . "/api/v1/fokabotMessage?k=" . $ScoresConfig["api_key"] . "&to=%23ranked-now&msg=" . $msgtoannounce . "";
 			} else if ($status == "unrank") {
 				$bm = $GLOBALS["db"]->fetch("SELECT beatmapset_id, song_name, bpm FROM beatmaps WHERE beatmapset_id = ? LIMIT 1", [$bsid]);
 				$msgtoannounce = "[https://osu.ppy.sh/s/" . $bsid . " " . $bm["song_name"] . "] just got unranked!";
 				$statuscrot = "Unranked";
 				$bpm = $bm["bpm"];
 				$namabm = $bm["song_name"];
-                $requestbro = $URL["bancho"] . "/api/v1/fokabotMessage?k=" . urlencode($ScoresConfig["api_key"]) . "&to=%23ranked-now&msg=" . $msgtoannounce . "";
+                		$requestbro = $URL["bancho"] . "/api/v1/fokabotMessage?k=" . $ScoresConfig["api_key"] . "&to=%23ranked-now&msg=" . $msgtoannounce . "";
 			}
 
 			$curl = new Curl();
