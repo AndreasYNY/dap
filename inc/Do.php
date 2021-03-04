23ff21f50f35f9a8a55ef597a0e368bbd322b9f9
unknown <jame_glove@yahoo.com>
2021-03-04 23:10:06 +0800

    null noll nell

--- inc/Do.php
+++ inc/Do.php
@@ -1047,7 +1047,7 @@ class D {
 			$months = giveDonor($_POST["id"], $_POST["m"], $_POST["type"] == 0);
 			$DCid = $GLOBALS["db"]->fetch("SELECT discord_tokens.userid, discord_tokens.token, users.username, discord_tokens.discord_id, discord_tokens.role_id FROM discord_tokens INNER JOIN users ON discord_tokens.userid=users.id WHERE discord_tokens.userid = ?
 			", [$_POST["id"]]);
-			if (!$DCid) {
+			if ($DCid['discord_id'] == NULL) {
 				$namaDonat = sprintf("%s", $uname);
 			} else {
 				$namaDonat = sprintf("<@%s>", $DCid['discord_id']);
