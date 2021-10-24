363382d361c83644b32d2c95a67995d027b9e2ad
troke12 <jame_glove@yahoo.com>
2021-10-24 19:48:42 +0800

    del: seasonal

--- seasonal.php
+++
@@ -1,8 +0,0 @@
-[
-    "https:\/\/assets.datenshi.pw\/seasonal\/default\/jqmcf1mru1v51.jpg",
-    "https:\/\/cdn.datenshi.pw\/static\/seasonal\/barbara2.png",
-    "https:\/\/assets.datenshi.pw\/seasonal\/default\/300457-mmk.png",
-    "https:\/\/assets.datenshi.pw\/seasonal\/default\/rankomanko.png",
-    "https:\/\/assets.datenshi.pw\/seasonal\/default\/eriri.jpg",
-    "https:\/\/assets.datenshi.pw\/seasonal\/default\/billing_warnet.jpg"
-]
